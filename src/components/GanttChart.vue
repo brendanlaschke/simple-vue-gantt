50f4e902ab096017a0635055003fc3635ed802d4
brendanlaschke <brendan.laschke@googlemail.com>
2026-04-29 08:17:12 +0200

    revert vite upgrade

--- src/components/GanttChart.vue
+++ src/components/GanttChart.vue
@@ -294,7 +294,6 @@ import {
 } from "@/utils/dateDifference";
 import { OverlayScrollbarsComponent } from "overlayscrollbars-vue";
 import type { OverlayScrollbars } from "overlayscrollbars";
-// @ts-expect-error - No types available for OverlayScrollbarsComponent
 import "overlayscrollbars/overlayscrollbars.css";
 import GanttSidebar from "./GanttSidebar.vue";
 import TimelineHeader from "./TimelineHeader.vue";
--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// @ts-expect-error - No types available for style.css
 import "./style.css";
 
 createApp(App).mount("#app");
